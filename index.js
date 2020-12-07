f861a474f8ec319f06539d076b952ae25ef3ef84
smokenet31 <agux2000@gmail.com>
2020-12-07 20:43:28 -0300

    second

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const
   express = require('express');
 
 var app = express();
-app.set('port', process.env.PORT || 5000);
+app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
